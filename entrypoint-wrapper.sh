fd7b07ad8b9f0b26a07212fad38d572a03464e29
MarcinMove37ai <167071023+MarcinMove37ai@users.noreply.github.com>
2025-06-25 22:45:43 +0200

    Update entrypoint-wrapper.sh

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -8,4 +8,4 @@ echo "Permissions fixed."
 # Używamy 'su' zamiast 'su-exec', ponieważ 'su' jest zawsze dostępne.
 # 'exec' na początku zastępuje proces skryptu procesem 'su', co jest dobrą praktyką.
 # 'su -s /bin/sh nobody -c 'exec "$@"'': Uruchamia polecenie przekazane jako argumenty ("$@") jako użytkownik 'nobody'.
-exec su -s /bin/sh nobody -c 'exec "$@"'
+exec su -s /bin/sh nobody -c "$@"
